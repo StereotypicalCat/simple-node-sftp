4d201878617a44c0a0c5bbd590c01dff2457dc04
Lucas Winther <lucasw89@live.dk>
2025-05-01 20:43:04 +0200

    Implement export functionality.

--- index.js.ts
+++ index.js.ts
@@ -1,17 +1,17 @@
 // @ts-ignore
 import Client from "ssh2-sftp-client";
-
 let sftp = new Client();
 
 import { program } from 'commander';
 import * as fs from "node:fs";
 import * as Path from "node:path";
+import {stat} from "node:fs";
 
 
 program.name('Simple Node SFTP')
     .description("A simple node program to push or pull fi+les using ssh2-sftp-client")
 
-program.command('push')
+program.command('upload')
     .description('upload files to a sftp server')
     .argument('<username>', 'username of sftp server')
     .argument('<password>', 'passwords of sftp server')
@@ -19,6 +19,18 @@ program.command('push')
     .argument('<port>', 'ports of sftp server')
     .argument('<src>', 'src path on local machine of files to upload')
     .argument('<dest>', 'dest path on remote server of files to upload')
+    .action((username: string, password: string, url: string, port: string, src: string, dest: string) => {
+        exporToSFTP(username, password, url, port, src, dest);
+    })
+
+program.command('download')
+    .description('download files from a sftp server')
+    .argument('<username>', 'username of sftp server')
+    .argument('<password>', 'passwords of sftp server')
+    .argument('<url>', 'url of sftp server')
+    .argument('<port>', 'ports of sftp server')
+    .argument('<src>', 'src path on remote machine of files to download')
+    .argument('<dest>', 'src path on local server of files to download')
     .action((username: string, password: string, url: string, port: string, src: string, dest: string) => {
         exporToSFTP(username, password, url, port, src, dest);
     })
@@ -74,6 +86,70 @@ const exporToSFTP = function (username: string, password: string, url: string, p
     }
 }
 
+const importFromSFTP = function (username: string, password: string, url: string, port: string, src: string, dest: string) {
 
+    // Check whether file or dir
+    let isDirectory: boolean = null
+
+    sftp.connect({
+        host: url,
+        port: port,
+        username: username,
+        password: password
+    }).then(() => {
+        let stats = sftp.stat(src)
+        isDirectory = stats.isDriectory
+    }).catch(err => {
+        console.log(err, 'catch error');
+    }).then(() => {
+        sftp.end();
+    })
+
+    console.log(isDirectory)
+
+    if (isDirectory === null){
+        console.log("The path you input is not exist!")
+        return
+    }
+
+    if (isDirectory){
+        // Connect!
+        console.log("Creating a connection to SFTP server...")
+
+        sftp.connect({
+            host: url,
+            port: port,
+            username: username,
+            password: password
+        }).then(() => {
+            return sftp.downloadDir(src, dest);
+        }).catch(err => {
+            console.log(err, 'catch error');
+        }).then(() => {
+            sftp.end();
+        })
+    }
+    else{
+        // Connect!
+        console.log("Creating a connection to SFTP server...")
+
+        const dirToCreate = Path.dirname(dest)
+        console.log(dirToCreate);
+        fs.mkdirSync(dirToCreate, { recursive: true });
+
+        sftp.connect({
+            host: url,
+            port: port,
+            username: username,
+            password: password
+        }).then(() => {
+            return sftp.get(src, dest)
+        }).catch(err => {
+            console.log(err, 'catch error');
+        }).then(() => {
+            sftp.end();
+        })
+    }
+}
 
 program.parse();
